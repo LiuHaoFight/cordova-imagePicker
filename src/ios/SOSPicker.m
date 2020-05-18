a597f4a1a35ec658cbcbaab2429e28834f08a490
lk W <wlkhn123@gmail.com>
2020-05-18 18:23:34 +0800

    adjust to select single one album for iOS

--- src/ios/SOSPicker.m
+++ src/ios/SOSPicker.m
@@ -13,55 +13,103 @@
 
 #define CDV_PHOTO_PREFIX @"cdv_photo_"
 
+@interface SOSPicker ()
+
+@property (nonatomic, strong) ALAssetsLibrary *specialLibrary;
+
+@property (nonatomic, copy) NSArray *chosenImages;
+
+@end
+
 @implementation SOSPicker
 
 @synthesize callbackId;
 
-- (void) getPictures:(CDVInvokedUrlCommand *)command {
-	NSDictionary *options = [command.arguments objectAtIndex: 0];
+- (void)getPictures:(CDVInvokedUrlCommand *)command {
+    NSDictionary *options = [command.arguments objectAtIndex:0];
 
-	NSInteger maximumImagesCount = [[options objectForKey:@"maximumImagesCount"] integerValue];
-	self.width = [[options objectForKey:@"width"] integerValue];
-	self.height = [[options objectForKey:@"height"] integerValue];
-	self.quality = [[options objectForKey:@"quality"] integerValue];
+    NSInteger maximumImagesCount = [[options objectForKey:@"maximumImagesCount"] integerValue];
+    self.width = [[options objectForKey:@"width"] integerValue];
+    self.height = [[options objectForKey:@"height"] integerValue];
+    self.quality = [[options objectForKey:@"quality"] integerValue];
 
-	// Create the an album controller and image picker
-	ELCAlbumPickerController *albumController = [[ELCAlbumPickerController alloc] init];
-	
-	if (maximumImagesCount == 1) {
-      albumController.immediateReturn = true;
-      albumController.singleSelection = true;
-   } else {
-      albumController.immediateReturn = false;
-      albumController.singleSelection = false;
-   }
-   
-   ELCImagePickerController *imagePicker = [[ELCImagePickerController alloc] initWithRootViewController:albumController];
-   imagePicker.maximumImagesCount = maximumImagesCount;
-   imagePicker.returnsOriginalImage = 1;
-   imagePicker.imagePickerDelegate = self;
+    self.callbackId = command.callbackId;
 
-   albumController.parent = imagePicker;
-	self.callbackId = command.callbackId;
-	// Present modally
-	[self.viewController presentViewController:imagePicker
-	                       animated:YES
-	                     completion:nil];
+    // Create the an album controller and image picker
+    ELCAlbumPickerController *albumController = [[ELCAlbumPickerController alloc] init];
+
+    if (maximumImagesCount == 1) {
+        albumController.immediateReturn = true;
+        albumController.singleSelection = true;
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+        self.specialLibrary = library;
+        NSMutableArray *groups = [NSMutableArray array];
+        [_specialLibrary enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos
+            usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+              if (group) {
+                  [groups addObject:group];
+              } else {
+                  // this is the end
+                  [self displayPickerForGroup:[groups objectAtIndex:0]];
+              }
+            }
+            failureBlock:^(NSError *error) {
+              self.chosenImages = nil;
+              UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:[NSString stringWithFormat:@"Album Error: %@ - %@", [error localizedDescription], [error localizedRecoverySuggestion]] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+              [alert show];
+
+              NSLog(@"A problem occured %@", [error description]);
+              // an error here means that the asset groups were inaccessable.
+              // Maybe the user or system preferences refused access.
+            }];
+    } else {
+        albumController.immediateReturn = false;
+        albumController.singleSelection = false;
+        ELCImagePickerController *imagePicker = [[ELCImagePickerController alloc] initWithRootViewController:albumController];
+        imagePicker.maximumImagesCount = maximumImagesCount;
+        imagePicker.returnsOriginalImage = 1;
+        imagePicker.imagePickerDelegate = self;
+
+        albumController.parent = imagePicker;
+
+        // Present modally
+        [self.viewController presentViewController:imagePicker
+                                          animated:YES
+                                        completion:nil];
+    }
 }
 
+- (void)displayPickerForGroup:(ALAssetsGroup *)group {
+    ELCAssetTablePicker *tablePicker = [[ELCAssetTablePicker alloc] initWithStyle:UITableViewStylePlain];
+    tablePicker.singleSelection = YES;
+    tablePicker.immediateReturn = YES;
+
+    ELCImagePickerController *elcPicker = [[ELCImagePickerController alloc] initWithRootViewController:tablePicker];
+    elcPicker.maximumImagesCount = 1;
+    elcPicker.imagePickerDelegate = self;
+    elcPicker.returnsOriginalImage = YES; //Only return the fullScreenImage, not the fullResolutionImage
+    tablePicker.parent = elcPicker;
+
+    // Move me
+    tablePicker.assetGroup = group;
+    [tablePicker.assetGroup setAssetsFilter:[ALAssetsFilter allAssets]];
+
+    [self.viewController presentViewController:elcPicker animated:YES completion:nil];
+}
 
 - (void)elcImagePickerController:(ELCImagePickerController *)picker didFinishPickingMediaWithInfo:(NSArray *)info {
-	CDVPluginResult* result = nil;
-	NSMutableArray *resultStrings = [[NSMutableArray alloc] init];
-    NSData* data = nil;
-    NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
-    NSError* err = nil;
-    NSFileManager* fileMgr = [[NSFileManager alloc] init];
-    NSString* filePath;
-    ALAsset* asset = nil;
-    UIImageOrientation orientation = UIImageOrientationUp;;
+    CDVPluginResult *result = nil;
+    NSMutableArray *resultStrings = [[NSMutableArray alloc] init];
+    NSData *data = nil;
+    NSString *docsPath = [NSTemporaryDirectory() stringByStandardizingPath];
+    NSError *err = nil;
+    NSFileManager *fileMgr = [[NSFileManager alloc] init];
+    NSString *filePath;
+    ALAsset *asset = nil;
+    UIImageOrientation orientation = UIImageOrientationUp;
+    ;
     CGSize targetSize = CGSizeMake(self.width, self.height);
-	for (NSDictionary *dict in info) {
+    for (NSDictionary *dict in info) {
         asset = [dict objectForKey:@"ALAsset"];
         // From ELCImagePickerController.m
 
@@ -69,11 +117,11 @@
         do {
             filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, CDV_PHOTO_PREFIX, i++, @"jpg"];
         } while ([fileMgr fileExistsAtPath:filePath]);
-        
+
         @autoreleasepool {
             ALAssetRepresentation *assetRep = [asset defaultRepresentation];
             CGImageRef imgRef = NULL;
-            
+
             //defaultRepresentation returns image as it appears in photo picker, rotated and sized,
             //so use UIImageOrientationUp when creating our image below.
             if (picker.returnsOriginalImage) {
@@ -82,15 +130,15 @@
             } else {
                 imgRef = [assetRep fullScreenImage];
             }
-            
-            UIImage* image = [UIImage imageWithCGImage:imgRef scale:1.0f orientation:orientation];
+
+            UIImage *image = [UIImage imageWithCGImage:imgRef scale:1.0f orientation:orientation];
             if (self.width == 0 && self.height == 0) {
-                data = UIImageJPEGRepresentation(image, self.quality/100.0f);
+                data = UIImageJPEGRepresentation(image, self.quality / 100.0f);
             } else {
-                UIImage* scaledImage = [self imageByScalingNotCroppingForSize:image toSize:targetSize];
-                data = UIImageJPEGRepresentation(scaledImage, self.quality/100.0f);
+                UIImage *scaledImage = [self imageByScalingNotCroppingForSize:image toSize:targetSize];
+                data = UIImageJPEGRepresentation(scaledImage, self.quality / 100.0f);
             }
-            
+
             if (![data writeToFile:filePath options:NSAtomicWrite error:&err]) {
                 result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:[err localizedDescription]];
                 break;
@@ -98,29 +146,27 @@
                 [resultStrings addObject:[[NSURL fileURLWithPath:filePath] absoluteString]];
             }
         }
+    }
 
-	}
-	
-	if (nil == result) {
-		result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:resultStrings];
-	}
+    if (nil == result) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:resultStrings];
+    }
 
-	[self.viewController dismissViewControllerAnimated:YES completion:nil];
-	[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+    [self.viewController dismissViewControllerAnimated:YES completion:nil];
+    [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
 }
 
 - (void)elcImagePickerControllerDidCancel:(ELCImagePickerController *)picker {
-	[self.viewController dismissViewControllerAnimated:YES completion:nil];
-	CDVPluginResult* pluginResult = nil;
-    NSArray* emptyArray = [NSArray array];
-	pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:emptyArray];
-	[self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
+    [self.viewController dismissViewControllerAnimated:YES completion:nil];
+    CDVPluginResult *pluginResult = nil;
+    NSArray *emptyArray = [NSArray array];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:emptyArray];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
 }
 
-- (UIImage*)imageByScalingNotCroppingForSize:(UIImage*)anImage toSize:(CGSize)frameSize
-{
-    UIImage* sourceImage = anImage;
-    UIImage* newImage = nil;
+- (UIImage *)imageByScalingNotCroppingForSize:(UIImage *)anImage toSize:(CGSize)frameSize {
+    UIImage *sourceImage = anImage;
+    UIImage *newImage = nil;
     CGSize imageSize = sourceImage.size;
     CGFloat width = imageSize.width;
     CGFloat height = imageSize.height;
